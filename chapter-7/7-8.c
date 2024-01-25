316133c5da2239e78e46ade414811969605f1906
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-01-25 18:42:42 +0530

    Update 7-8.c

--- chapter-7/7-8.c
+++ chapter-7/7-8.c
@@ -1,27 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 int main()
 {
-	int line = 0,page=1,pr=0;
-	FILE *fp;
-	fp=fopen("pqr.txt","r");
-	char c;
-	while((c=fgetc(fp))!=EOF)
-	{
-		if (line==0 && pr==0)
-		{
-			printf("\n---START OF PAGE %d---\n\n",page);
-			pr=1;
-		}
-		if(c=='\n') line++;
-		putchar(c);
-		if(line==5) 
-		{
-			printf("\n---END OF PAGE %d---\n",page);
-			line =0;
-			pr=0;
-			page++;
-		}
-	}
+    int line = 0, page = 1, pr = 0;
+    FILE *fp;
 
+    
+    fp = fopen("pqr.txt", "r"); // Open the file for reading
+	
+    if (fp == NULL)  // Check if the file opened successfully
+    {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    char c;
+
+    
+    while ((c = fgetc(fp)) != EOF) // Loop through each character in the file
+    {
+        
+        if (line == 0 && pr == 0) // Check if it's the start of a new page
+        {
+            printf("\n---START OF PAGE %d---\n\n", page);
+            pr = 1;
+        }
+
+       
+        if (c == '\n')  // if the character is a newline character increment line
+            line++;
+        putchar(c);
+
+        
+        if (line == 5) // Check if we have reached the end of a page
+        {
+            printf("\n---END OF PAGE %d---\n", page);
+            line = 0; // Reset line count and page flag
+            pr = 0;
+            page++;
+        }
+    }
+
+    fclose(fp);
+
+    return 0;
 }

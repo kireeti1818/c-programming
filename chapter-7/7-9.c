3f76e6bf41baf2480676902b1313d01fc4d368e8
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-01-25 18:40:29 +0530

    Update 7-9.c

--- chapter-7/7-9.c
+++ chapter-7/7-9.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+
+// Function to check if a character is an upper charcter or not
 int isupper1(char c) {
-    if (c < 'A' || c > 'Z')
-        return 0;
+    if (c < 'A' || c > 'Z')   
+        return 0;               // Return 0 if not an uppercase letter
     else
-        return 1;
-}
-int main() 
-{
-    char c;
-    while ((c = getchar()) != EOF && c!='\n') 
-    {
-        if (isupper1(c) == 1)
-            printf("true\n");
-        else
-            printf("false\n");
-    }
-    return 0;
+        return 1;               // Return 1 if an uppercase letter
 }
 
+int main() {
+    char c;
+
+    while ((c = getchar()) != EOF && c != '\n') {
+        if (isupper1(c) == 1)
+            printf("true\n");   
+        else
+            printf("false\n");  /
+    }
+
+    return 0;
+}

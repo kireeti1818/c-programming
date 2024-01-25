cd4391f11aa00e7f8ef69524abda04df3f935515
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-01-25 18:47:31 +0530

    Update 7-6.c

--- chapter-7/7-6.c
+++ chapter-7/7-6.c
@@ -6,7 +6,7 @@ int main()
 	fp1=fopen("abc.txt","r");
 	fp2=fopen("xyz.txt","w");
 	char c;
-	while((c=fgetc(fp1))!=EOF)
+	while((c=fgetc(fp1))!=EOF)     // Read characters from "abc.txt" and write to "xyz.txt"
 	{
 		fputc(c,fp2);
 	}

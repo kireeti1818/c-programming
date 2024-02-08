9522003cf5c64ae9bb47b4622d3c755134acf1cd
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-02-08 10:34:12 +0530

    Update server.c

--- socket_programming/echo server/server.c
+++ socket_programming/echo server/server.c
@@ -15,6 +15,7 @@ int main(int argc, char const *argv[])
 	int port=30233;
 	char *address="127.0.0.1";
 
+	//creating socket
 	int sockfd=socket(AF_INET,SOCK_STREAM,0);
 	if (sockfd<0)
 	{
@@ -23,7 +24,7 @@ int main(int argc, char const *argv[])
 	}
 	printf("socket created\n");
 
-
+	//enable reuse of local addresses 
 	int yes=1;
 	if(setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int))==-1)
 	{
@@ -38,7 +39,7 @@ int main(int argc, char const *argv[])
 	server.sin_addr.s_addr=inet_addr(address);
 	server.sin_port=htons(port);
 
-
+	//associates socket with a specific network address and port number.
 	int bin =bind(sockfd,(struct sockaddr *)&server,sizeof(server));
 	if (bin<0)
 	{
@@ -47,9 +48,11 @@ int main(int argc, char const *argv[])
 	}
 	printf("binding port \n");
 
-
+	//make the specified socket a listening socket, allowing it to accept incoming connections.
+	//'1' indicates the maximum length of the queue of pending connections
 	int lis=listen(sockfd,1);
 
+	//allowing it to accept incoming connections.
 	socklen_t len=sizeof(client);
 	int acc;
 	if ((acc = accept(sockfd, (struct sockaddr *)&client, &len)) < 0)
@@ -59,25 +62,28 @@ int main(int argc, char const *argv[])
 	}
 	printf("accepting the connection\n");
 
+
 	char buff[1000];
 	memset(&buff,'\0',1000);
+
+	//receving message from the server
 	int rec;
 	rec=recv(acc,buff,1000,0);
 
-
+	//sending the received message to client as a response by attaching time stamp
 	time_t cur_time;
 	time(&cur_time);
 	printf("Message recieved : %s at %s",buff,ctime(&cur_time));
 	strcat(buff," - ");
 	strcat(buff,ctime(&cur_time));
 	printf("%s",buff);
-
-
 	int n=send(acc,buff,strlen(buff),0);
 	if(n<0)
 	{
 		printf("error in sending\n");
 	}
+
+	//closing the acc and sockfd
 	close(acc);
 	close(sockfd);
 	return 0;

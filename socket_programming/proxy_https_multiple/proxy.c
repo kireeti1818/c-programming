9a8b38ea41547f7427c781c65ad863e362a65399
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-02-26 18:41:32 +0530

    Update proxy.c

--- socket_programming/proxy_https_multiple/proxy.c
+++ socket_programming/proxy_https_multiple/proxy.c
@@ -12,13 +12,11 @@
 
 #define PORT 8010
 #define MAX_LINE 10000
-
-void function(int client_sock)
+void functionToHandleRequest(int client_sock)
 {
   struct addrinfo server_conn_addr,*result;
-
   char buffer[MAX_LINE];
-  ssize_t r;
+  int r;
   if ((r = recv(client_sock, buffer, MAX_LINE,0)) > 0)
   printf("request Received: \n\n%s\n", buffer);
   char *ge= strstr(buffer, "GET");
@@ -31,7 +29,7 @@ void function(int client_sock)
   {
     return;
   }
-  if(strstr(buffer,"mozilla")!=NULL) return;
+  // if(strstr(buffer,"mozilla")!=NULL) return;
   char *conn = strstr(buffer, "CONNECT");
   char host_addr[1000];
   char host_port[7];
@@ -41,122 +39,108 @@ void function(int client_sock)
      char *host_start = buffer + strlen("CONNECT") + 1;
      char *host_end = strstr(host_start, " ");
      //printf("Im here 1\n" );
-
      if (host_end != NULL)
      {
-
        //printf("IM 2");
-         strncpy(host_addr, host_start, host_end - host_start);
-         host_addr[host_end - host_start] = '\0';
-         char *port_str = strchr(host_addr, ':');
-         if (port_str != NULL)
-        {
-             strcpy(host_port, port_str + 1);
-             *port_str = '\0';
-         }
-         else
-         {
-             strcpy(host_port, "443");
-         }
-         printf("Host: %s\n", host_addr);
-         printf("Port: %s\n", host_port);
+       strncpy(host_addr, host_start, host_end - host_start);
+       host_addr[host_end - host_start] = '\0';
+       char *port_str = strchr(host_addr, ':');
+       if (port_str != NULL)
+       {
+           strcpy(host_port, port_str + 1);
+           *port_str = '\0';
+       }
+       else
+       {
+           strcpy(host_port, "443");
+       }
+       printf("Host: %s\n", host_addr);
+       printf("Port: %s\n", host_port);
      }
   }
   int n;
-  if((n = getaddrinfo(host_addr, host_port, &server_conn_addr, &result)) != 0)  // gets address information of the server
+  if((n = getaddrinfo(host_addr, host_port, &server_conn_addr, &result)) != 0)
   {
-    perror("Getaddrinfo error...\n");
+    perror("Getaddrinfo");
     exit(1);
   }
   int server_conn;
-  for(struct addrinfo *p = result; p != NULL; p = p-> ai_next)  // to get the perfect address to connect
+  struct addrinfo *x;
+  for(x = result; x != NULL; x = x-> ai_next)
   {
-    server_conn = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    server_conn = socket(x->ai_family, x->ai_socktype, x->ai_protocol);
     if(server_conn < 0)
     {
-      perror("Socket creation error...\n");
+      perror("Socket");
       printf("\n");
       continue;
     }
      printf("[+]socket created\n\n");
-    if(connect(server_conn, p->ai_addr, p->ai_addrlen) < 0)
+    if(connect(server_conn, x->ai_addr, x->ai_addrlen) < 0)
     {
       close(server_conn);
-      perror("Connection error...\n");
+      perror("Connect");
       printf("\n");
       continue;
     }
     printf("[+]connection established\n\n");
-    printf("%s\n", inet_ntoa(((struct sockaddr_in *)(p->ai_addr))->sin_addr));
+    printf("%s\n", inet_ntoa(((struct sockaddr_in *)(x->ai_addr))->sin_addr));
     break;
   }
-  char header[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length:1024\r\n\r\n";
-  send(client_sock, header, strlen(header), 0);  // we send the success message to the browser/client t
+  char header[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  send(client_sock, header, strlen(header), 0);
+
   printf("[+]Connected to server\n");
+
   while (1)
   {
-    struct pollfd fds[2];
-    fds[0].fd = client_sock;
-    fds[0].events = POLLIN;
+    	struct pollfd fds[2];
+    	fds[0].fd = client_sock;
+    	fds[0].events = POLLIN;
 
-    fds[1].fd = server_conn;
-    fds[1].events = POLLIN;
+    	fds[1].fd = server_conn;
+    	fds[1].events = POLLIN;
 
-    int ret = poll(fds, 2, -1);
+    	int ret = poll(fds, 2, -1);
 
-    if (ret < 0)
-    {
-          perror("Poll error \n");
-          exit(1);
+    	if (ret < 0)
+    	{
+        		perror("Poll");
+        		exit(1);
+    	}
+
+   	for (int j = 0; j < 2; j++)
+   	{
+    		if (fds[j].revents & POLLIN)
+    		{
+        		char bufferclient[MAX_LINE];
+        		int rece;
+            if (j==0)
+            {
+              rece=recv( client_sock , bufferclient, sizeof(bufferclient), 0);
+              if (rece<= 0) break;
+              else printf("recived :-> %s\n",bufferclient);
+              send( server_conn , bufferclient, rece, 0);
+
+            }
+            else
+            {
+              rece=recv(  server_conn, bufferclient, sizeof(bufferclient), 0);
+              if (rece<= 0) break;
+              else printf("recived :-> %s\n",bufferclient);
+              send( client_sock, bufferclient,rece, 0);
+            }
+    		}
+    	}
     }
-
-    for (int fd = 0; fd < 2; fd++)
-    {
-      if (fds[fd].revents & POLLIN)
-      {
-          char buffer[MAX_LINE];
-          ssize_t received;
-          if(fd==0)
-          {
-            recv(client_sock , buffer, sizeof(buffer), 0);
-          }
-          else
-          {
-            recv(server_conn, buffer, sizeof(buffer), 0);
-          }
-          if (received <= 0)
-          {
-                break;
-          }
-
-
-
-          if(fd==0)
-          {
-            send( server_conn, buffer, received, 0);          }
-          else
-          {
-            send(client_sock, buffer, received, 0);
-          }
-
-      }
-    }
-  }
-
-    close(client_sock);
-    close(server_conn);
-
-  return;
+  	close(client_sock);
+  	close(server_conn);
 }
-
-
 
 int main()
 {
   struct sockaddr_in server, client;
   int server_sock;
-
-
   if((server_sock=socket(AF_INET,SOCK_STREAM,0))<0)
   {
     perror("Error in socket");
@@ -184,8 +168,6 @@ int main()
     perror("Error in bind");
     exit(0);
   }
-
-
   if(listen(server_sock,10)<0)
   {
     perror("Error in listen");
@@ -195,39 +177,33 @@ int main()
   socklen_t client_len = sizeof(client);
   while(1)
   {
-
     if((client_sock=accept(server_sock, (struct sockaddr*)&client,&client_len)) == -1)
     {
         perror("Error in accept");
         exit(1);
     }
-
-
     pid_t pid;
+    /*fork() is used to create a new process by duplicating the current process.
+    After a fork, two processes are created: the parent process and the child process.
+    The child process is an exact copy of the parent process*/
     if ((pid=fork()) ==-1)
     {
       printf("pid : -1");
-
-        perror("Error in Fork");
-        close(client_sock);
-        continue;
+      perror("Error in Fork");
+      close(client_sock);
+      continue;
     }
-    else if(pid==0) //child process
+    else if(pid==0) // for child
     {
       close(server_sock);
-      //https://www.google.com/
-      function(client_sock);
+      functionToHandleRequest(client_sock);
       exit(1);
-
-
     }
-    else //parent process
+    else // for parent
     {
-      // printf("pid : 1");
       close(client_sock);
     }
   }
   close(server_sock);
   return 0;
 }
-
